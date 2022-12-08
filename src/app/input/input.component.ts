748dff33140c1764bc046eecb81d851037eea956
Don Ismael <amine.nemmaoui@student.vinci.be>
2022-12-08 21:14:27 +0100

    merge

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -6,6 +6,7 @@ import { FormControl } from '@angular/forms';
   templateUrl: './input.component.html',
   styles: []
 })
+
 export class InputComponent {
   @Input() inputControl!: FormControl;
   @Input() nameLabel!: string;
