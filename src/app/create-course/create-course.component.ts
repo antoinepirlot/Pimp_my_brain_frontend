79223828b59f362d13ad265e105bfb97c6b80617
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-09 17:37:19 +0100

    Add verify no empty field

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -16,10 +16,24 @@ export class CreateCourseComponent {
     price: new FormControl('', Validators.required),
     description: new FormControl('', Validators.required)
   });
-  
+
 
   onSubmit() {
-    console.log("soumission")
-    console.log(this.createCourseForm.value.optionChosen);
+    //verify that there is no empty field
+    if (this.createCourseForm.value.country?.trim().length == 0 ||
+      this.createCourseForm.value.city?.trim().length == 0 ||
+      this.createCourseForm.value.optionChosen?.trim().length == 0 ||
+      this.createCourseForm.value.description?.trim().length == 0 ||
+      this.createCourseForm.value.price?.length===0
+    ) {
+      this.notification = "Vous devez remplir tous les champs du formulaire"
+      return;
+    }
+    if(Number.isNaN(Number(this.createCourseForm.value.price))) {
+      this.notification = "Vous devez entrer un nombre dans le champ prix"
+      return;
+    }
+    //TODO : call the backend to add course
+    //TODO : go to the homepage
   }
 }
