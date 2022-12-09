f1b44e670e6ce84e86ee8e8150a171673a298879
Stefan <stefan.mircovici@student.vinci.be>
2022-12-09 14:45:25 +0100

    [REF] add contructor with http client into auth service

--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthentificationService{
 
+    constructor(private http: HttpClient){}
 }
