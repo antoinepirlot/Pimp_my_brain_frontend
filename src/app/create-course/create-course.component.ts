bd1cf2f0f504569e07386a9658b77e590d82c56c
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-10 21:47:18 +0100

    Add categoryservice

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Category } from '../models/category';
+import { CategoriesService } from '../services/categories.service';
 
 @Component({
   selector: 'app-create-course',
   templateUrl: './create-course.component.html',
   styleUrls: ['./create-course.component.css']
 })
-export class CreateCourseComponent {
+export class CreateCourseComponent implements OnInit {
   notification = ""
-  allOptions = ["PHP", "Python", "SQL"] // TODO : ask categories to the backend
+  allOptionsCategory:Category[] = []//["PHP", "Python", "SQL"] // TODO : ask categories to the backend
+  allOptions:string[] = []
   createCourseForm = new FormGroup({
     country: new FormControl('', Validators.required),
     city: new FormControl('', Validators.required),
@@ -16,6 +19,30 @@ export class CreateCourseComponent {
     price: new FormControl('', Validators.required),
     description: new FormControl('', Validators.required)
   });
+
+  constructor(private categoriesService :CategoriesService){
+
+  }
+
+  ngOnInit(){
+    this.categoriesService.getAllCategories().subscribe({
+      next : (data) => {
+        this.allOptionsCategory = data
+      },
+      error : (error) => {
+        if(error.status === 404){
+          this.notification = "L'email ou le mot de passe est incorrect";
+        }
+        else{
+          console.warn("Server error");
+        }
+      }
+    });
+      //cat => this.allOptionsCategory = cat);
+    console.log(this.allOptionsCategory)
+    console.log("stefan")
+    this.allOptionsCategory.forEach(c => this.allOptions.push(c.name))
+  }
 
 
   onSubmit() {
