fe859ab490c467c0ce7af84d39097f7e47e0249a
Antoine Pirlot <pirlot.antoine@outlook.com>
2022-12-10 12:23:25 +0100

    Add : doc from original function

--- src/app/utils/handle_error.ts
+++ src/app/utils/handle_error.ts
@@ -1,5 +1,12 @@
 import {Observable, of} from "rxjs";
 
+/**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ *
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
 export function handleError<T>(operation = 'operation', result?: T) {
   return (error: any): Observable<T> => {
 
