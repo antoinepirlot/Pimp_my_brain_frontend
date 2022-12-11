f49f329dd47670e03b718d18d3a1b8ebc06e6ef1
Stefan <stefan.mircovici@student.vinci.be>
2022-12-11 23:24:28 +0100

    [REF] refactor 3 card column into 4

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -21,14 +21,17 @@ export class HomeComponent implements OnInit {
       next: (data) => {
         if(data.length===0) return;
 
+
+        let numberOfColumns = 4;
         //Separate the courses in lists of 3 courses
-        for(let i = 0; i< data.length ; i=i+3){
+        for(let i = 0; i< data.length ; i=i+numberOfColumns){
           let dataSeparatedInBloc:Course[]  = new Array<Course>();  
-          let end = i+3;
+          let end = i+numberOfColumns;
           for(let j = i; j< data.length && j<end ; j++){
             dataSeparatedInBloc.push(data[j]);
           }
           this.courses.push(dataSeparatedInBloc);
+          console.log(dataSeparatedInBloc)
         }
       },
       error: (error) => {
