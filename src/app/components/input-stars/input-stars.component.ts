91c70d224d3f279480ed21fa74830acb605ecba8
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-12 22:10:20 +0100

    add input stars component

---
+++ src/app/components/input-stars/input-stars.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-stars',
+  templateUrl: 'input-stars.component.html', 
+  styleUrls: ['input-stars.component.css']
+})
+export class InputStarsComponent {
+
+}
