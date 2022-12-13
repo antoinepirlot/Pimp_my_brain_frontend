222659e49d8d3f79cbcd0985f2b5fbee8f34fad1
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-13 10:42:32 +0100

    Add rating in profile page

--- src/app/pages/public/profile/profile.component.ts
+++ src/app/pages/public/profile/profile.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Rating } from 'src/app/models/rating';
 import { User } from 'src/app/models/user';
+import { RatingsService } from 'src/app/services/ratings.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -17,12 +19,21 @@ export class ProfileComponent {
     numberRating: new FormControl(1, Validators.required)
   });
   notificationAddRating: string = "";
-  constructor(private route: ActivatedRoute, private usersService: UserService) {
+
+  newRating: Rating = {
+    rating_text: "",
+    rating_number: 1,
+    id_rater: 2, //TODO : change id with the person connected
+    id_rated: this.idUser
+  };
+
+  constructor(private route: ActivatedRoute, private usersService: UserService, private ratingsService: RatingsService) {
 
   }
 
   ngOnInit() {
     this.idUser = +this.route.snapshot.params['id_user'];
+    this.newRating.id_rated = this.idUser;
     this.usersService.getUserById(this.idUser).subscribe({
       next: (data) => {
         this.userProfile = data
@@ -32,9 +43,6 @@ export class ProfileComponent {
   }
 
   addRating() {
-    console.log("add rating")
-    console.log(this.ratingForm!.get("numberRating")!.value!)
-    console.log(this.ratingForm.get("descriptionRating")?.value)
     if (this.ratingForm!.get("numberRating")!.value! <= 0 ||
       this.ratingForm!.get("numberRating")!.value! > 5) {
       this.notificationAddRating = "Le nombre d'étoiles entré est incorrecte";
@@ -45,10 +53,33 @@ export class ProfileComponent {
       return;
     }
 
+    this.newRating.rating_text = this.ratingForm!.get("descriptionRating")!.value!.trim()
+    this.newRating.rating_number = this.ratingForm!.get("numberRating")!.value!
+    console.log(this.newRating)
 
-    //TODO : add rating
 
-    this.notificationAddRating = ""
+    //add rating
+    this.ratingsService.createOneRating(this.newRating).subscribe({
+      next: (data) => {
+        this.notificationAddRating = "La note a bien été publiée !"
+        this.ratingForm!.get("descriptionRating")!.setValue("");
+      },
+      error: (error) => {
+        if (error.status === 409) {
+          this.notificationAddRating = "Vous avez déjà publié un avis pour ce professeur";
+        } else if (error.status === 404) {
+          this.notificationAddRating = "Le professeur n'est plus présent sur notre site"
+        } else if (error.status === 403) {
+          this.notificationAddRating = "Il faut avoir fini un cours avec un professeur pour lui mettre un avis"
+        } else {
+          console.warn("Server error");
+        }
+      }
+    });
+
+
+
+
 
   }
 
