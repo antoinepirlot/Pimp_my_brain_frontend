d38d7a826ce7db0773322c3a79be465f61ec7838
Stefan <stefan.mircovici@student.vinci.be>
2022-12-14 01:53:18 +0100

    [REF] refactor sumbit button in search bar homepage

--- src/app/components/submit-button/submit-button.component.ts
+++ src/app/components/submit-button/submit-button.component.ts
@@ -3,8 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-submit-button',
   templateUrl: './submit-button.component.html',
-  styleUrls: []
+  styleUrls: ['./submit-button.component.css']
 })
 export class SubmitButtonComponent {
   @Input() value!: string;
+  @Input() class?: string;
 }
