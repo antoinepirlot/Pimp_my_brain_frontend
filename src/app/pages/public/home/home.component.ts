f93684590842317f4d702da6b81962876ce7acf7
Stefan <stefan.mircovici@student.vinci.be>
2022-12-14 12:43:25 +0100

    [REF] add ... if the description card is too long

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -45,12 +45,17 @@ export class HomeComponent implements OnInit {
       next: (data) => {
         if(data.length===0) return;
 
+        
+
         let numberOfColumns = 4;
         //Separate the courses in lists of 3 courses
         for(let i = 0; i< data.length ; i=i+numberOfColumns){
           let dataSeparatedInBloc:Course[]  = new Array<Course>();  
           let end = i+numberOfColumns;
           for(let j = i; j< data.length && j<end ; j++){
+            if(data[j].course_description.length>113){
+              data[j].course_description=data[j].course_description.substring(0,113) + "...";
+            }
             dataSeparatedInBloc.push(data[j]);
           }
           this.courses.push(dataSeparatedInBloc);
